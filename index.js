618aecdd5712afe2ea5b8d4ccdb798b18afd8b4b
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-02-06 20:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -131,11 +131,11 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame(function() {
+  window.requestAnimationFrame(function(){
     const left = positionToInteger(DODGER.style.left)
 
-    if (left > 0) {
-      DODGER.style.left = `$(left - 4)px`
+    if (left > 0){
+      DODGER.style.left = `${left - 4}px`
     }
   })
 }
